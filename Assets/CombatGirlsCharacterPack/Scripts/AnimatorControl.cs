cd805a0ed4bb6c6f3821f92062f3e97c1e1dd2a8
YoungDogg <leejonghwo@gmail.com>
2026-03-11 10:58:45 +0900

    외부 에셋 팩을 저장소에 포함한다
    다른 환경에서도 플레이어, 드래곤, 투사체, TMP 폰트 리소스가 누락되지 않도록 CombatGirlsCharacterPack, FourEvilDragonsPBR, UNI VFX, TextMesh Pro 에셋을 저장소에 추가한다. .gitignore 에서 외부 에셋 제외 규칙을 제거해 필요한 리소스를 함께 추적하도록 정리한다.

---
+++ Assets/CombatGirlsCharacterPack/Scripts/AnimatorControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CombatGirlsCharacterPack
+{
+    public class AnimatorControl : MonoBehaviour
+    {
+        private Animator animator;
+        public Toggle rootMotionToggle; // Toggle UI ������Ʈ�� �����մϴ�.
+
+        private void Start()
+        {
+            // ĳ���� �������� Animator ������Ʈ�� �����մϴ�.
+            animator = GetComponent<Animator>();
+
+            // ��� UI�� ���¸� ������ ������ �Լ��� ȣ���մϴ�.
+            rootMotionToggle.onValueChanged.AddListener(ToggleRootMotion);
+        }
+
+        public void ToggleRootMotion(bool enableRootMotion)
+        {
+            // ���ö��� ��Ʈ��� �ɼ��� �����մϴ�.
+            animator.applyRootMotion = enableRootMotion;
+        }
+    }
+}
---
+++ Assets/CombatGirlsCharacterPack/Scripts/Camera_Work_Script/CameraWalk.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CombatGirlsCharacterPack
+{
+    public class CameraWalk : MonoBehaviour
+    {
+        [Header("ī�޶� �̵��ӵ�")]
+        public float moveSpeed = 10.0f;
+        [Header("ī�޶� ȸ�� ����(���콺)")]
+        public float rotateSpeed = 500.0f;
+        [Header("ī�޶� �� �ӵ�")]
+        public float zoomSpeed = 10.0f;
+        [Header("ī�޶� �� �ּ�/�ִ�")]
+        public float minFov = 15.0f;
+        public float maxFov = 90.0f;
+
+        protected bool isCursorVisible = true;
+
+        protected virtual void Start()
+        {
+
+        }
+
+        protected virtual void Update()
+        {
+            myCameraWalk();
+        }
+        protected virtual void myCameraWalk()
+        {
+            // WASD�� ī�޶� �̵�
+            float horizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+            float vertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+            transform.Translate(horizontal, 0, vertical);
+            if (Input.GetKey(KeyCode.Q))
+            {
+                transform.Translate(0, -moveSpeed * Time.deltaTime, 0);
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+            }
+            // ���콺 �ٷ� Ȯ��/���
+            float fov = GetComponent<Camera>().fieldOfView;
+            fov -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+            fov = Mathf.Clamp(fov, minFov, maxFov);
+            GetComponent<Camera>().fieldOfView = fov;
+
+            // ������ ���콺�� ���콺 Ŀ�� ǥ��/�����
+            VisibleMouse();
+
+            // 
+            if (!isCursorVisible)
+            {
+                float mouseX = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+                float mouseY = Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x - mouseY, transform.eulerAngles.y + mouseX, 0);
+            }
+        }
+        protected void VisibleMouse()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                isCursorVisible = !isCursorVisible;
+                Cursor.visible = isCursorVisible;
+                Cursor.lockState = isCursorVisible ? CursorLockMode.None : CursorLockMode.Locked;
+            }
+        }
+    }
+}
